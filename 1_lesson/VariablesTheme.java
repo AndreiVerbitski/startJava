4696a2ff35717a6acf4aa963cd148548011bdd55
Andrei Verbitskii <andrew162555@gmail.com>
2022-07-04 02:33:57 +0300

    Второе исправление

--- 1_lesson/VariablesTheme.java
+++ 1_lesson/VariablesTheme.java
@@ -1,7 +1,6 @@
 public class VariablesTheme {
 
     public static void main(String[] args) {
-
         System.out.println("\n1. Создание переменных и вывод их значений на консоль");
 
         byte core = 8;
@@ -27,16 +26,17 @@ public class VariablesTheme {
         int x =100;
         int y = 200;
         int allItems = x + y;
-        double sale = 0.11;
-        double summaSkidki = allItems * sale;
-        double total = allItems + summaSkidki;
+        double amountSale = allItems * 0.11;
+        double total = allItems + amountSale;
 
-        System.out.println("Summa skidki: " + summaSkidki);
-        System.out.println("Itogo: " + total);
+        System.out.println("Сумма скидки " + amountSale);
+        System.out.println("Итого: " + total);
 
         System.out.println("\n3. Вывод на консоль слова JAVA");
 
-        System.out.println("\n   J    a  v     v  a    \n   J   a a  v   v  a a   \nJ  J  aaaaa  V V  aaaaa  \n     JJ  a     a  V  a     a");
+        System.out.println("\n   J    a  v     v  a    \n   " +
+                "J   a a  v   v  a a   \nJ  J  aaaaa  V V  aaaaa  \n " +
+                "JJ  a     a  V  a     a");
 
         System.out.println("\n4. Отображение min и max значений числовых типов данных");
 
@@ -45,36 +45,25 @@ public class VariablesTheme {
         int intMax = 2147483646;
         long longMax = 9223372036854775807L;
 
-        byte byteMaxDecr = (byte) (byteMax + 1);
-        byte byteMaxIncr = (byte) (byteMax - 1);
+        System.out.println("Первоначальное значение byte: " + byteMax);
+        System.out.println("Значение после декремента byte: " + (byteMax + 1));
+        System.out.println("Значение после инкремента byte: " + (byteMax - 1));
 
-        short shortMaxDecr = (short) (shortMax + 1);
-        short shortMaxIncr = (short) (shortMax -1);
+        System.out.println("Первоначальное значение short: " + shortMax);
+        System.out.println("Значение после декремента short: " + (shortMax + 1));
+        System.out.println("Значение после инкремента short: " + (shortMax -1));
 
-        int intMaxDecr = (int) (intMax + 1);
-        int intMaxIncr = (int) (intMax - 1);
+        System.out.println("Первоначальное значение int: " + intMax);
+        System.out.println("Значение после декремента int: " + (intMax + 1));
+        System.out.println("Значение после инкремента int: " + (intMax - 1));
 
-        long longMaxDecr = (long) (longMax + 1);
-        long longMaxIncr = (long) (longMax - 1);
-
-        System.out.println("Original value byte: " + byteMax);
-        System.out.println("Decrement value byte: " + byteMaxDecr);
-        System.out.println("Increment value byte: " + byteMaxIncr);
-    
-        System.out.println("Original value short: " + shortMax);
-        System.out.println("Decrement value short: " + shortMaxDecr);
-        System.out.println("Increment value short: " + shortMaxIncr);
-
-        System.out.println("Original value int: " + intMax);
-        System.out.println("Decrement value int: " + intMaxDecr);
-        System.out.println("Increment value int: " + intMaxIncr);
-
-        System.out.println("Original value long: " + longMax);
-        System.out.println("Decrement value long: " + longMaxDecr);
-        System.out.println("Increment value long: " + longMaxIncr);
+        System.out.println("Первоначальное значение long: " + longMax);
+        System.out.println("Значение после декремента long: " + (longMax + 1));
+        System.out.println("Значение после инкремента long: " + (longMax - 1));
 
         System.out.println("\n5. Перестановка значений переменных");
 
+        System.out.println("С помощью третьей переменной");
         double numb1 = 1.1;
         double numb2 = 2.2;
         double numb3;
@@ -86,92 +75,83 @@ public class VariablesTheme {
         System.out.println(numb1);
         System.out.println(numb2);
 
-        double numb4 = 1.1;
-        double numb5 = 2.2;
+        System.out.println("с помощью арифметических операций");
+        numb1 = numb1 - numb2;
+        numb2 = numb2 + numb1 ;
 
-        numb4 += 1.1;
-        numb5 -= 1.1;
+        System.out.println(numb1);
+        System.out.println(numb2);
 
-        System.out.println(numb4);
-        System.out.println(numb5);
+        System.out.println("с помощью побитовой операции");
+        numb1 = (int) numb1 ^ (int) numb2;
+        numb2 = ((int) numb1 ^ (int) numb2) + 0.1;
+        numb1 = ((int) numb1 ^ (int) numb2) + 0.2;
 
-        double a = 1.1;
-        double b = 2.2;
+        System.out.println(numb1);
+        System.out.println(numb2);
 
-        a = (int)a ^ (int)b;
-        b = ((int)a ^ (int)b) + 0.1;
-        a = ((int)a ^ (int)b) + 0.2;
-        System.out.println(a);
-        System.out.println(b);
+
 
         System.out.println("\n6. Вывод символов и их кодов");
 
-        char ch1 = 35;
-        char ch2 = 38;
-        char ch3 = 64;
-        char ch4 = 94;
-        char ch5 = 95;
+        char codeChar35 = 35;
+        char codeChar38 = 38;
+        char codeChar64 = 64;
+        char codeChar94 = 94;
+        char codeChar95 = 95;
 
-        int numb1Ch = ch1;
-        int numb2Ch = ch2;
-        int numb3Ch = ch3;
-        int numb4Ch = ch4;
-        int numb5Ch = ch5;
-
-        System.out.println("Symbol code " + numb1Ch + " equal " + ch1);
-        System.out.println("Symbol code " + numb2Ch + " equal " + ch2);
-        System.out.println("Symbol code " + numb3Ch + " equal " + ch3);
-        System.out.println("Symbol code " + numb4Ch + " equal " + ch4);
-        System.out.println("Symbol code " + numb5Ch + " equal " + ch5);
+        System.out.println("Символ кода " + ((int)codeChar35) + " равен " + codeChar35);
+        System.out.println("Символ кода " + ((int)codeChar38) + " равен " + codeChar38);
+        System.out.println("Символ кода " + ((int)codeChar64) + " равен " + codeChar64);
+        System.out.println("Символ кода " + ((int)codeChar94) + " равен " + codeChar94);
+        System.out.println("Символ кода " + ((int)codeChar95) + " равен " + codeChar95);
 
         System.out.println("\n7. Произведение и сумма цифр числа");
 
-        int number = 345;
+        int srcNum = 345;
 
-        System.out.println(number % 11); // ravno 4
-        System.out.println(number % 6); // ravno 3
-        System.out.println(number % 17); // ravno 5
+        System.out.println(srcNum / 100); // ravno 3
+        System.out.println((srcNum / 10) % 10) ; // ravno 4
+        System.out.println((srcNum % 10)); // ravno 5
 
-        int number_1 = number % 6;
-        int number_2 = number % 11;
-        int number_3 = number % 17;
-        int summaNumbers = number_1 + number_2 + number_3;
-        int multiplyNumbers = number_1 * number_2 * number_3;
-        System.out.println("Summa chisel 3 4 5 = " + summaNumbers);
-        System.out.println("Proizdenie chisel 3 4 5 = " + multiplyNumbers);
+
+        int number_1 = srcNum / 100;
+        int number_2 = (srcNum / 10) % 10;
+        int number_3 = (srcNum % 10);
+        int sumDigits = number_1 + number_2 + number_3;
+        int multDigits = number_1 * number_2 * number_3;
+        System.out.println("Сумма чисел 3 4 5 = " + sumDigits);
+        System.out.println("Произведение чисел 3 4 5 = " + multDigits);
 
         System.out.println("\n8. Вывод на консоль ASCII-арт Дюка");
 
-        char sym1 = 47;
-        char sym2 = 92;
-        char sym3 = 95;
-        char sym4 = 40;
-        char sym5 = 41;
+        char slash = 47;
+        char backSlash = 92;
+        char lowerSlash = 95;
+        char leftBrace = 40;
+        char rightBrace = 41;
+        char emptySpace = 32;
 
-        System.out.println("    " + sym1 + sym2 + "         ");
-        System.out.println("   " + sym1 + "  " + sym2 + "       ");
-        System.out.println("  " + sym1 + sym3 + sym4 + " " + sym5 + sym2 + "            ");
-        System.out.println(" " + sym1 + "      " + sym2);
-        System.out.println(sym1 + " " + sym3 + sym3 + sym3 + sym1 + sym2 + sym3 + sym3 + sym2);
+        System.out.println(slash + slash + slash + slash);
 
         System.out.println("\n9. Отображение количества сотен, десятков и единиц числа");
 
-        int chislo = 123;
+        int srcNum1 = 123;
 
-        System.out.println("Number " + chislo + " contains " + (chislo % 2) + " soten");
-        System.out.println("Number " + chislo + " contains " + (chislo % 37) + " desyatkov") ; // 1
-        System.out.println("Number " + chislo + " contains " + chislo + " edenic");
+        System.out.println("Число " + srcNum1 + " содержит " + (srcNum1 / 100) + " сотен");
+        System.out.println("Число " + srcNum1 + " содержит " + (srcNum1 / 10 ) + " десятков");
+        System.out.println("Число " + srcNum1 + " содержит " + (srcNum1 % 10 ) + " единиц");
 
         System.out.println("\n10. Преобразование секунд");
 
-        int seconds = 86399;
+        int secconds = 86399;
 
-        int ostatokSeconds = seconds % 60;
-        int minutes = seconds / 60;
-        int ostatokMinutes = minutes % 60;
-        int hours = seconds / 3600;
+        int leftSec = secconds % 60;
+        int minutes = secconds / 60;
+        int leftMin = minutes % 60;
+        int hours = secconds / 3600;
 
-        System.out.println(hours + ":" + ostatokMinutes + ":" + ostatokSeconds);
+        System.out.println(hours + ":" + leftMin + ":" + leftSec);
 
     }
 }
