a1648c973ef23993029269a148f89804a8ced57b
Andrei Verbitskii <andrew162555@gmail.com>
2022-07-10 00:12:06 +0300

    2 задание

--- 1_lesson/IfElseStatementTheme.java
+++ 1_lesson/IfElseStatementTheme.java
@@ -50,19 +50,23 @@ public class IfElseStatementTheme {
 
         System.out.println("\n3. Работа с числом");
 
-        int srcNum = 23;
+        System.out.println("\n3. Работа с числом");
 
-        if (srcNum == 0) {
-        } else if (srcNum % 2 == 0) {
-            System.out.println("Число " + srcNum + " чётное");
+        int srcNum = 1612;
+
+        if(srcNum != 0) {
+            if(srcNum > 0) {
+                System.out.printf("Число %d положительное\n", srcNum);
+            } else {
+                System.out.printf("Число %d отрицательное\n", srcNum);
+            }
+            if(srcNum % 2 == 0) {
+                System.out.printf("Число %d четное\n", srcNum);
+            } else {
+                System.out.printf("Число %d нечетное\n", srcNum);
+            }
         } else {
-            System.out.println("Число " + srcNum +  " нечетное");
-        }
-        if (srcNum == 0) {
-        } else if (srcNum < 0) {
-            System.out.println("Число " + srcNum + " является отрицательным");
-        } else if (srcNum > 0) {
-            System.out.println("Число " + srcNum + " является положительным");
+            System.out.printf("Число %d это ноль\n", srcNum);
         }
 
         System.out.println("\n4. Поиск одинаковых цифр в числах");
@@ -87,14 +91,14 @@ public class IfElseStatementTheme {
 
         System.out.println("\n5. Определение буквы, числа или символа по их коду");
 
-        char someChar = '\u0057';
+        char codeChar = '\u0057';
 
-        if (someChar >= 'a' && someChar <= 'z') {
-            System.out.println(someChar + " Это маленькая буква");
-        } else if (someChar >= 'A' && someChar <= 'Z') {
-            System.out.println(someChar + " Это большая буква");
-        } else if (someChar == Integer.valueOf(someChar)) {
-            System.out.println(someChar + " Это число");
+        if (codeChar >= 'a' && codeChar <= 'z') {
+            System.out.println(codeChar + " Это маленькая буква");
+        } else if (codeChar >= 'A' && codeChar <= 'Z') {
+            System.out.println(codeChar + " Это большая буква");
+        } else if (codeChar >= '0' && codeChar <= '9') {
+            System.out.println(codeChar + " Это число");
         } else {
             System.out.println("Это не буква и не число");
         }
@@ -102,20 +106,19 @@ public class IfElseStatementTheme {
         System.out.println("\n6. Определение суммы вклада и начисленных банком %");
 
         int contribution = 300000;
-        double percentContr = 0;
+        double percent = 0;
 
         if (contribution < 100000) {
-            percentContr = contribution * 0.05;
+            percent = 0.05;
         } else if (contribution >= 100000 && contribution <= 300000) {
-            percentContr = contribution * 0.07;
+            percent = 0.07;
         } else if (contribution > 300000) {
-            percentContr = contribution * 0.10;
+            percent = 0.10;
         }
 
-        int totalAmount = contribution + (int) percentContr;
         System.out.println("Сумма вклада " + contribution);
-        System.out.println("Начисленный % " + (int) percentContr);
-        System.out.println("Итоговая сумма с % " + totalAmount);
+        System.out.println("Начисленный % " + (contribution * percent));
+        System.out.println("Итоговая сумма с % " + ((contribution * percent) + contribution));
 
         System.out.println("\n7. Определение оценки по предметам\n");
 
@@ -125,31 +128,23 @@ public class IfElseStatementTheme {
         int avrgeBallProgramming = 0;
 
         if (percHistory > 91) {
-            avrgeBallHistory = 5;
-            System.out.println("Итоговая оценка по истории " + avrgeBallHistory);
+            System.out.println("Оценка по истории " + (avrgeBallHistory = 5));
         } else if (percHistory > 73) {
-            avrgeBallHistory = 4;
-            System.out.println("Итоговая оценка по истории " + avrgeBallHistory);
+            System.out.println("Оценка по истории " + (avrgeBallHistory = 4));
         } else if (percHistory > 60) {
-            avrgeBallHistory = 3;
-            System.out.println("Итоговая оценка по истории "  + avrgeBallHistory);
+            System.out.println("Оценка по истории " + (avrgeBallHistory = 3));
         } else if (percHistory < 60) {
-            avrgeBallHistory = 2;
-            System.out.println("Итоговая оценка по истории "  + avrgeBallHistory);
+            System.out.println("Оценка по истории " + (avrgeBallHistory = 2));
         }
 
         if (percProgramming > 91) {
-            avrgeBallProgramming = 5;
-            System.out.println("Итоговая оценка по программированию " + avrgeBallProgramming);
+            System.out.println("Оценка по истории " + (avrgeBallProgramming = 5));
         } else if (percProgramming > 73) {
-            avrgeBallProgramming = 4;
-            System.out.println("Итоговая оценка по программированию " + avrgeBallProgramming);
+            System.out.println("Оценка по истории " + (avrgeBallProgramming = 4));
         } else if (percProgramming > 60) {
-            avrgeBallProgramming = 3;
-            System.out.println("Итоговая оценка по программированию " + avrgeBallProgramming);
+            System.out.println("Оценка по истории " + (avrgeBallProgramming = 3));
         } else if (percProgramming < 60) {
-            avrgeBallProgramming = 2;
-            System.out.println("Итоговая оценка по программированию " + avrgeBallProgramming);
+            System.out.println("Оценка по истории " + (avrgeBallProgramming = 2));
         }
 
         int avrgeBallSubjects = (avrgeBallHistory + avrgeBallProgramming) / 2;
@@ -160,9 +155,9 @@ public class IfElseStatementTheme {
         System.out.println("\n8. Расчет прибыли");
 
         int rentMonth = 5000;
-        int saleMonth = 13000;
-        int costOfItems = 9000;
-        int profitInYear = (saleMonth * 12) - ((rentMonth + costOfItems) * 12);
+        int incomeMonth = 13000;
+        int primeCost = 9000;
+        int profitInYear = ((incomeMonth - rentMonth - primeCost) * 12);
 
         if (profitInYear > 0) {
             System.out.println("Прибыль за год: +" + profitInYear);
@@ -189,7 +184,7 @@ public class IfElseStatementTheme {
             dozensInUnits = (needDozens - currentDozens) * 10;
             needUnits = needUnits + dozensInUnits;
         }
-        
+
         System.out.println("Выдать " + needHundreads + " сотен");
         System.out.println("Выдать " + currentDozens + " десятков");
         System.out.println("Выдать " + needUnits + " единиц");
