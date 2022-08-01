a45e2dd74c4f367c4a006700afe198476e169cc4
Andrei Verbitskii <andrew162555@gmail.com>
2022-08-01 20:54:15 +0300

    Правка задачи по массивам.

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -1,13 +1,11 @@
 package lesson_2_3_4.array;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Random;
 
 public class ArrayTheme {
 
     public static void main(String[] args) {
-        System.out.println("1. Реверс значений массива\n");
+        System.out.println("\n1. Реверс значений массива");
 
         int[] intArr = {1, 2, 3, 4, 5, 6, 7};
         System.out.print("Массив до модификации: ");
@@ -15,51 +13,32 @@ public class ArrayTheme {
         for (int number : intArr) {
             System.out.print(number + " ");
         }
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = intArr.length - i;
+        }
 
-        intArr[0] = intArr.length;
-        intArr[1] = intArr.length - 1;
-        intArr[2] = intArr.length - 2;
-        intArr[3] = intArr.length - 3;
-        intArr[4] = intArr.length - 4;
-        intArr[5] = intArr.length - 5;
-        intArr[6] = intArr.length - 6;
-
-        System.out.println();
-        System.out.print("Массив после модификации: ");
+        System.out.print("\nМассив после модификации: ");
 
         for (int number : intArr) {
             System.out.print(number + " ");
         }
 
         System.out.println("\n\n2. Вывод произведения элементов массива");
-
         intArr = new int[10];
+
         for (int i = 0; i < intArr.length; i++) {
             intArr[i] = i;
         }
 
         int multiplyNumbers = 1;
-        String showZeroOrnine = "";
 
         for (int i = 1; i < intArr.length - 1; i++) {
             multiplyNumbers *= intArr[i];
+            String showResult = (i != 8) ? intArr[i] + " * " : intArr[i] + " = " + multiplyNumbers;
+            System.out.print(showResult);
         }
-
-//        for (int i = 0; i < intArr.length; i++) {
-//            showZeroOrnine = intArr[i] == 0 ? intArr[i] + "\n" : intArr[i] + " * ";
-//            System.out.printf("%s",showZeroOrnine);
-//        }
-
-        for (int number : intArr) {
-            if (number == 0) System.out.println(intArr[0]);
-            else if (number == 8) {
-                System.out.print(number + " = " + multiplyNumbers);
-            } else if (number == 9) {
-                System.out.println("\n" + intArr[9]);
-            } else {
-                System.out.print(number + " * ");
-            }
-        }
+        System.out.println("\nИндекс 0: " + intArr[0]);
+        System.out.println("Индекс 9: " + intArr[9]);
 
         System.out.println("\n3. Удаление элементов массива");
         double[] doubleArr = new double[15];
@@ -68,9 +47,7 @@ public class ArrayTheme {
             doubleArr[i] = Math.random();
         }
 
-        int middleIndexArray = (doubleArr.length - 1) / 2;
-
-        System.out.println("Исходный маасив:");
+        System.out.println("Исходный масcив:");
 
         for (int i = 0; i < doubleArr.length; i++) {
             if (i == 8) {
@@ -83,9 +60,11 @@ public class ArrayTheme {
         System.out.println();
         System.out.println("Изменённый массив:");
 
+        int middleIndexArray = 7;
         int scoreZero = 0;
+
         for (int i = 0; i < doubleArr.length; i++) {
-            if (doubleArr[middleIndexArray] < doubleArr[i]) {
+            if (doubleArr[i] > doubleArr[middleIndexArray]) {
                 scoreZero++;
                 doubleArr[i] = 0;
             }
@@ -101,9 +80,9 @@ public class ArrayTheme {
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
 
         char[] engSymb = new char[26];
-        System.out.println(engSymb.length);
         int i = 65;
         int count = 0;
+
         while (i <= 90) {
             engSymb[count] = (char) i;
             i++;
@@ -111,10 +90,10 @@ public class ArrayTheme {
         }
 
         for (i = engSymb.length; i >= 0; i--) {
-            System.out.println();
             for (int j = engSymb.length - 1; j >= i; j--) {
                 System.out.print(engSymb[j]);
             }
+            System.out.println();
         }
 
         System.out.println("\n5. Генерация уникальных чисел");
@@ -154,8 +133,30 @@ public class ArrayTheme {
         }
 
         System.out.println();
-        for (int numb : intArr) {
-            System.out.print(numb + " ");
+        for (i = 0; i < intArr.length; i++) {
+            if (i == 9 || i == 19) {
+                System.out.printf("%2d", intArr[i]);
+                System.out.println();
+            } else System.out.printf("%2d ", intArr[i]);
+        }
+
+        System.out.println("\n\n6. Сдвиг элементов массива");
+
+        String[] strArr = {"", "AA", "", "BBB", "CC", "D", "", "E", "FF", "G", ""};
+        String[] copyStrArr = new String[7];
+
+        System.arraycopy(strArr, 1, copyStrArr, 0, 1);
+        System.arraycopy(strArr, 3, copyStrArr, 1, 3);
+        System.arraycopy(strArr, 7, copyStrArr, 4, 3);
+
+        for (String str : strArr) {
+            System.out.print(str + " ");
+        }
+
+        System.out.println();
+
+        for (String str : copyStrArr) {
+            System.out.print(str + " ");
         }
     }
 }
