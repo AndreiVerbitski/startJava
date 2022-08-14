9917747877e35b9f5ccb0a6533b4c9beaf5411aa
Andrei Verbitskii <andrew162555@gmail.com>
2022-08-14 17:48:47 +0300

    Задания со звёздачками f,g (Калькулятор)

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,20 +2,19 @@ package lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public static String calculate(String mathExpression) {
+    public static int calculate(String mathExpression) {
         String[] fullExpression = mathExpression.split(" ");
-        String result;
         int a = Integer.parseInt(fullExpression[0]);
         int b = Integer.parseInt(fullExpression[2]);
         char sign = fullExpression[1].charAt(0);
-        result = switch (sign) {
-            case '+' -> String.valueOf(Math.addExact(a, b));
-            case '-' -> String.valueOf(Math.subtractExact(a, b));
-            case '*' -> String.valueOf(Math.multiplyExact(a, b));
-            case '/' -> String.valueOf(Math.floorDiv(a, b));
-            case '%' -> String.valueOf(a % b);
-            case '^' -> String.valueOf((int) Math.pow(a, b));
-            default -> "Введите корректный оператор";
+        int result = switch (sign) {
+            case '+' -> Math.addExact(a, b);
+            case '-' -> Math.subtractExact(a, b);
+            case '*' -> Math.multiplyExact(a, b);
+            case '/' -> Math.floorDiv(a, b);
+            case '%' -> a % b;
+            case '^' -> (int) Math.pow(a, b);
+            default -> throw new IllegalStateException("Unexpected value: " + a);
         };
         return result;
     }
