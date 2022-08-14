487f3ef2b6885d4a098c26e7b63373b0897ed8b2
Andrei Verbitskii <andrew162555@gmail.com>
2022-08-14 17:44:17 +0300

    Крайняя правка калькулятора.

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,25 +2,21 @@ package lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public int calculate(String mathExpression) {
+    public static String calculate(String mathExpression) {
         String[] fullExpression = mathExpression.split(" ");
+        String result;
         int a = Integer.parseInt(fullExpression[0]);
         int b = Integer.parseInt(fullExpression[2]);
         char sign = fullExpression[1].charAt(0);
-        switch (sign) {
-            case '+':
-                return Math.addExact(a, b);
-            case '-':
-                return Math.subtractExact(a, b);
-            case '*':
-                return Math.multiplyExact(a, b);
-            case '/':
-                return Math.floorDiv(a, b);
-            case '%':
-                return a % b;
-            case '^':
-                return (int) Math.pow(a, b);
-        }
-        return 0;
+        result = switch (sign) {
+            case '+' -> String.valueOf(Math.addExact(a, b));
+            case '-' -> String.valueOf(Math.subtractExact(a, b));
+            case '*' -> String.valueOf(Math.multiplyExact(a, b));
+            case '/' -> String.valueOf(Math.floorDiv(a, b));
+            case '%' -> String.valueOf(a % b);
+            case '^' -> String.valueOf((int) Math.pow(a, b));
+            default -> "Введите корректный оператор";
+        };
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,11 +2,12 @@ package lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
+import static lesson_2_3_4.calculator.Calculator.calculate;
+
 public class CalculatorTest {
 
     public static void main(String[] args) {
         System.out.println("\n1. Калькулятор:");
-        Calculator calc = new Calculator();
         Scanner console = new Scanner(System.in);
         String answer = "yes";
 
@@ -14,7 +15,7 @@ public class CalculatorTest {
             if (answer.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
                 String mathExpression = console.nextLine();
-                System.out.println(calc.calculate(mathExpression));
+                System.out.println(calculate(mathExpression));
             }
             System.out.print("Хотите продолжить вычисления? [yes/no] ");
             answer = console.nextLine();
