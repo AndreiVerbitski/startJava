197e65a7918de0e00ef0af6c3fed65443d649f04
Andrei Verbitskii <andrew162555@gmail.com>
2022-08-14 22:30:40 +0300

    Правка по последнему замечанию и выполнение остальных заданий со * (Калькулятор)

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -3,19 +3,38 @@ package lesson_2_3_4.calculator;
 public class Calculator {
 
     public static int calculate(String mathExpression) {
-        String[] fullExpression = mathExpression.split(" ");
-        int a = Integer.parseInt(fullExpression[0]);
-        int b = Integer.parseInt(fullExpression[2]);
-        char sign = fullExpression[1].charAt(0);
-        int result = switch (sign) {
-            case '+' -> Math.addExact(a, b);
-            case '-' -> Math.subtractExact(a, b);
-            case '*' -> Math.multiplyExact(a, b);
-            case '/' -> Math.floorDiv(a, b);
-            case '%' -> a % b;
-            case '^' -> (int) Math.pow(a, b);
-            default -> throw new IllegalStateException("Unexpected value: " + sign);
-        };
-        return result;
+        String[] partExpression = mathExpression.split(" ");
+        int a = 0;
+        int b = 0;
+        if (!partExpression[0].contains(".")) {
+            if (Integer.parseInt(partExpression[0]) >= 0) {
+                a = Integer.parseInt(partExpression[0]);
+                if (!partExpression[2].contains(".")) {
+                    if (Integer.parseInt(partExpression[2]) >= 0) {
+                        b = Integer.parseInt(partExpression[2]);
+                        char sign = partExpression[1].charAt(0);
+                        return switch (sign) {
+                            case '+' -> Math.addExact(a, b);
+                            case '-' -> Math.subtractExact(a, b);
+                            case '*' -> Math.multiplyExact(a, b);
+                            case '/' -> Math.floorDiv(a, b);
+                            case '%' -> a % b;
+                            case '^' -> (int) Math.pow(a, b);
+                            default -> throw new IllegalStateException("Unexpected value: " + sign);
+                        };
+                    } else {
+                        b = Integer.parseInt(partExpression[2]);
+                        throw new IllegalStateException("Unexpected value: " + b + "\n введите правильные значения");
+                    }
+                } else {
+                    throw new IllegalStateException("Unexpected value: " + partExpression[2]);
+                }
+            } else {
+                a = Integer.parseInt(partExpression[0]);
+                throw new IllegalStateException("Unexpected value: " + a + "\n введите правильные значения");
+            }
+        } else {
+            throw new IllegalStateException("Unexpected value: " + partExpression[0]);
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,24 +1,31 @@
 package lesson_2_3_4.calculator;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-
-import static lesson_2_3_4.calculator.Calculator.calculate;
 
 public class CalculatorTest {
 
     public static void main(String[] args) {
+        boolean tryCatch = true;
         System.out.println("\n1. Калькулятор:");
         Scanner console = new Scanner(System.in);
         String answer = "yes";
-
-        do {
-            if (answer.equals("yes")) {
-                System.out.print("Введите математическое выражение: ");
-                String mathExpression = console.nextLine();
-                System.out.println(calculate(mathExpression));
+        while (tryCatch) {
+            try {
+                do {
+                    if (answer.equals("yes")) {
+                        System.out.print("Введите математическое выражение: ");
+                        String mathExpression = console.nextLine();
+                        System.out.println(Calculator.calculate(mathExpression));
+                    }
+                    System.out.print("Хотите продолжить вычисления? [yes/no] ");
+                    answer = console.nextLine();
+                    tryCatch = false;
+                } while (!"no".equals(answer));
+            } catch (IllegalStateException e) {
+                System.out.println("Вы ввели не правильное значение, попробуйте ещё раз.");
+                tryCatch = true;
             }
-            System.out.print("Хотите продолжить вычисления? [yes/no] ");
-            answer = console.nextLine();
-        } while (!"no".equals(answer));
+        }
     }
 }
